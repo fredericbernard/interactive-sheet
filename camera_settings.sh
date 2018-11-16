1578d0d713a81f76e7a95ea0c884585445802af9
OLBEA20 <olivier.beaulieu.7@ulaval.ca>
2018-11-16 09:40:00 -0500

    camera settings

---
+++ camera_settings.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+uvcdynctrl --device=video1 -s 'Brightness' 137 
+uvcdynctrl --device=video1 -s 'White Balance Temperature, Auto' 1 
+uvcdynctrl --device=video1 -s 'Contrast' 35
+uvcdynctrl --device=video1 -s 'Saturation' 47
+uvcdynctrl --device=video1 -s 'Sharpness' 255 
+uvcdynctrl --device=video1 -s 'Exposure, Auto' 3
+uvcdynctrl --device=video1 -s 'Exposure, Auto Priority' 0
+uvcdynctrl --device=video1 -s 'Backlight Compensation' 1
+uvcdynctrl --device=video1 -s 'Focus' 0
