27bd70ae1f9a8d450c49930d3b5d7fc02a936ae4
OLBEA20 <olivier.beaulieu.7@ulaval.ca>
2018-12-14 09:32:58 -0500

    calibration

--- vision_project/vision/aruco_calibrator.py
+++ vision_project/vision/aruco_calibrator.py
@@ -9,7 +9,7 @@ import numpy as np
 
 from numpy.linalg import inv
 from PIL import ImageTk
-from cv2 import aruco
+import cv2.aruco as aruco
 from jivago.lang.annotations import Inject
 
 import infra
@@ -45,27 +45,28 @@ class ArucoCalibrator(object):
         return corners
 
 
+def write_distance_calibration_file(inverse_rotation_matrix, inverse_camera_matrix, translation_vector):
+    data = {'inverse_rotation_matrix': np.asarray(inverse_rotation_matrix).tolist(), 'inverse_camera_matrix': np.asarray(inverse_camera_matrix).tolist(), 'tvec': np.asarray(translation_vector).tolist()}
+
+    with open(os.path.join(os.path.dirname(vision_project.vision.__file__), 'config/distance_calibration.yaml', 'w')) as f:
+        yaml.dump(data, f)
+
 def calibrate(corners_on_image, corners_on_projector):
     with open(os.path.join(os.path.dirname(vision_project.vision.__file__), 'config/camera_calibration.yaml')) as f:
         loaded_dict = yaml.load(f)
 
-    mtx = loaded_dict.get('camera_matrix')
-    mtx = np.asarray(mtx)
-    dist = loaded_dict.get('dist_coeff')
-    dist = np.asarray(dist)
-
-    ret, rvec, tvec = cv2.solvePnP(corners_on_projector, corners_on_image, mtx, dist)
-    rotation_vector = cv2.Rodrigues(rvec)[0]
-    rotation_matrix = np.matrix(rotation_vector)
-    cameraMatrix = np.matrix(mtx)
-
-    iR = inv(rotation_matrix)
+    distance_coefficients = loaded_dict.get('dist_coeff')
+    distance_coefficients = np.asarray(distance_coefficients)
+    camera_matrix = loaded_dict.get('camera_matrix')
+    camera_matrix = np.asarray(camera_matrix)
+    cameraMatrix = np.matrix(camera_matrix)
     iC = inv(cameraMatrix)
 
-    data = {'inverse_rotation_matrix': np.asarray(iR).tolist(), 'inverse_camera_matrix': np.asarray(iC).tolist(), 'tvec': np.asarray(tvec).tolist()}
-
-    with open(os.path.join(os.path.dirname(vision_project.vision.__file__), 'config/distance_calibration.yaml', 'w')) as f:
-        yaml.dump(data, f)
+    ret, rvec, tvec = cv2.solvePnP(corners_on_projector, corners_on_image, camera_matrix, distance_coefficients)
+    rotation_vector = cv2.Rodrigues(rvec)[0]
+    rotation_matrix = np.matrix(rotation_vector)
+    iR = inv(rotation_matrix)
+    write_distance_calibration_file(iR, iC, tvec)
 
 
 if __name__ == '__main__':
--- vision_project/vision/coordinate_converter.py
+++ vision_project/vision/coordinate_converter.py
@@ -1,4 +1,3 @@
-import cv2
 import yaml
 import numpy as np
 import os
@@ -12,37 +11,26 @@ class DistanceCalculator:
     def __init__(self):
         config_file_path = os.path.dirname(vision_project.vision.__file__)
         with open(os.path.join(config_file_path, 'config/distance_calibration.yaml')) as distance_calibration_file:
-            camera_calibration = yaml.load(distance_calibration_file)
+            distance_calibration = yaml.load(distance_calibration_file)
         with open(os.path.join(config_file_path, 'config/camera_calibration.yaml')) as camera_calibration_file:
-            distance_calibration = yaml.load(camera_calibration_file)
+            camera_calibration = yaml.load(camera_calibration_file)
 
-        self.inverse_rotation_matrix = camera_calibration.get('inverse_rotation_matrix')
+        self.inverse_rotation_matrix = distance_calibration.get('inverse_rotation_matrix')
         self.inverse_rotation_matrix = np.asarray(self.inverse_rotation_matrix)
 
-        self.camera_matrix = distance_calibration.get('camera_matrix')
-        self.camera_matrix = np.asarray(self.camera_matrix)
-        self.inverse_camera_matrix = camera_calibration.get('inverse_camera_matrix')
-
+        self.inverse_camera_matrix = distance_calibration.get('inverse_camera_matrix')
         self.inverse_camera_matrix = np.asarray(self.inverse_camera_matrix)
 
-        self.distance_coefficients = distance_calibration.get('dist_coeff')
+        self.distance_coefficients = camera_calibration.get('dist_coeff')
         self.distance_coefficients = np.asarray(self.distance_coefficients)
 
-        self.translation_vector = camera_calibration.get('tvec')
+        self.translation_vector = distance_calibration.get('tvec')
         self.translation_vector = np.asarray(self.translation_vector)
 
-    def convert_undistorted_pixel_x_y_to_real_point(self, x, y):
+    def camera_coordinate_to_projector_coordinate(self, x, y):
         uv_point = np.array([x, y, np.ones(shape=1)]).reshape(3, 1)
         t = self.inverse_rotation_matrix.dot(self.inverse_camera_matrix).dot(uv_point)
         t2 = self.inverse_rotation_matrix.dot(self.translation_vector)
         s = (t2[2]) / t[2]
         object_point = (self.inverse_rotation_matrix.dot(s.item(0) * self.inverse_camera_matrix.dot(uv_point) - self.translation_vector))
         return WorldCoordinate(object_point.item(0), object_point.item(1))
-
-    def convert_undistorted_point_x_y_to_pixel(self, point):
-        uv_point = np.array([point.get_x().to_millimeters(), point.get_y().to_millimeters(), np.zeros(shape=1)]).reshape(1, 3)
-        data = cv2.projectPoints(uv_point, self.rvec, self.translation_vector, self.camera_matrix, self.distance_coefficients)[0]
-        x = data[0][0][0]
-        y = data[0][0][1]
-        return x, y
-
